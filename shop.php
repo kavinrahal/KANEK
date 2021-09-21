19cf4e3d91b5be6f2cc851030870ad27003f961b
kyguy1021 <90548175+kyguy1021@users.noreply.github.com>
2021-09-21 19:12:21 +1000

    changed logout button
    move logout button

--- shop.php
+++ shop.php
@@ -65,6 +65,7 @@
                                 <option>Sign In</option>
                             </select>
                         </div>
+                        <li class="nav-item"><a class="nav-link logout" href="logout.php">Logout</a></li>
                     </ul>
                 </div>
                 <!-- /.navbar-collapse -->
@@ -72,7 +73,6 @@
                 <!-- Start Atribute Navigation -->
                 <div class="attr-nav">
                     <ul>
-                        <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
                         <li class="side-menu">
 							<a href="#">
 								<i class="fa fa-shopping-bag"></i>
